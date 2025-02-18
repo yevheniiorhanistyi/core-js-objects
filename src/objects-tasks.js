50c12d51b67144e21f4a97b0af2b16bfc8fa1924
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-02-18 18:53:20 +0100

    feat: add 'isEmptyObject' solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -84,8 +84,8 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
 }
 
 /**
